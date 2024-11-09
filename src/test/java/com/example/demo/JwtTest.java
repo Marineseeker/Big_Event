e3831be92a38c03a22d23cc52a8ca8ec3c842982
Marine1651 <123360929+Marineseeker@users.noreply.github.com>
2024-11-09 14:28:14 +0800

    Big_Event

--- src/test/java/com/example/demo/JwtTest.java
+++ src/test/java/com/example/demo/JwtTest.java
@@ -26,20 +26,20 @@ public class JwtTest {
         System.out.println(token);
     }
 
-    @Test
-    public void testPass() {
-                        //头
-        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
-                        //载荷
-                "eyJ1c2VyIjoi5p2O5rW36ZizIiwiZXhwIjoxNzI5NDA4NDA0fQ." +
-                        //签名
-                "HNRiHn3WWSVkNQqobmoF0RfTdU5R_I0D_hZwYfIrka8";
-        //生成验证器
-        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256("Marine")).build();
-        //验证token
-        DecodedJWT decodedJWT = jwtVerifier.verify(token);
-        Map<String, Claim> claims = decodedJWT.getClaims();
-        System.out.println(claims.get("user").asString());
-        //token 密钥 过期时间 缺一不可
-    }
+//    @Test
+//    public void testPass() {
+//                        //头
+//        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+//                        //载荷
+//                "eyJ1c2VyIjoi5p2O5rW36ZizIiwiZXhwIjoxNzI5NDA4NDA0fQ." +
+//                        //签名
+//                "HNRiHn3WWSVkNQqobmoF0RfTdU5R_I0D_hZwYfIrka8";
+//        //生成验证器
+//        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256("Marine")).build();
+//        //验证token
+//        DecodedJWT decodedJWT = jwtVerifier.verify(token);
+//        Map<String, Claim> claims = decodedJWT.getClaims();
+//        System.out.println(claims.get("user").asString());
+//        //token 密钥 过期时间 缺一不可
+//    }
 }
